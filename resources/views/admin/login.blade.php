e6c8a2d6214d7d1b704e5f10792113455716e5b4
Jay-Ubisse <joaquimubisse@gmail.com>
2023-09-27 07:24:25 +0200

    setup tailwindcss, daisyui and organize views structure

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Postcode-mz</title>
+
+        @vite('resources/css/app.css')
+
+    </head>
+    <body class="bg-gradient-to-tr from-b">
+        
+    </body>
+</html>
